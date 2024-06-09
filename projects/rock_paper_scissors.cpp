d9615456720e83dff3e4adb8a66a85609c6465cb
2salted <noamgraf123@gmail.com>
2024-06-09 23:50:25 +0000

    update

--- projects/rock_paper_scissors.cpp
+++ projects/rock_paper_scissors.cpp
@@ -4,21 +4,14 @@
 
 int main() {
   std::string userAttack;
-  std::cout << "Enter your input (rock, paper or scissor): ";
+  std::cout << "Enter your input (rock, paper or scissors): ";
   std::cin >> userAttack;
-
-  if (userAttack != "rock" || "paper" || "scissor") {
-    std::cout << "learn how to spell idiot!" << std::endl;
-    return 0;
-  }
 
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<> distrib(1, 3);
   int AIAttack = distrib(gen);
-
-  if (AIAttack == 1) {
-  }
+  std::cout << AIAttack;
 
   return 0;
 }
