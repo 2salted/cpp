637531678361fa25ac212e674402e3a662b09346
2salted <noamgraf123@gmail.com>
2024-06-12 15:33:51 +0000

    added some files

--- projects/hangman.cpp
+++ projects/hangman.cpp
@@ -2,12 +2,29 @@
 #include <string>
 
 int main() {
-  std::string pickedWord = "funny", FoundLetters = "";
+  std::string pickedWord = "funny", FoundLetters;
   int attemptsLeft = 10;
   int wordLength = pickedWord.length();
 
   std::cout << "You have " << attemptsLeft << " attempts to guess a word with "
-            << wordLength << " letters\n";
+            << wordLength << " letters\n\n";
+
+  srand(time(0));
+  unsigned int randStartingLetter = rand() % wordLength;
+
+  std::cout << "Your first letter:\n";
+
+  for (unsigned int i = 0; i < pickedWord.length(); ++i) {
+    if (i == randStartingLetter) {
+      std::cout << pickedWord[i];
+      FoundLetters += pickedWord[i];
+    } else {
+      std::cout << " __ ";
+      FoundLetters += " __ ";
+    }
+  }
+
+  std::cout << "\n Found letters: " << FoundLetters << "\n";
 
   std::string pickedLetter;
   std::cout << "\nPick a letter (a-z): ";
@@ -20,11 +37,14 @@ int main() {
 
   for (unsigned int i = 0; i < pickedWord.length(); ++i) {
     if (pickedLetter[0] == pickedWord[i]) {
+      std::cout << pickedWord[i];
       FoundLetters += pickedWord[i];
     } else {
+      std::cout << " __ ";
       FoundLetters += " __ ";
     }
   }
+  std::cout << "\n Found letters: " << FoundLetters << "\n";
 
   return 0;
 }
