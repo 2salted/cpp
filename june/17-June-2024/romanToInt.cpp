496ca9d5d4fb7eba9ca2918eb457d04236618c7c
2salted <noamgraf123@gmail.com>
2024-06-17 22:22:25 +0000

    roman num to integer completed

--- june/17-June-2024/romanToInt.cpp
+++ june/17-June-2024/romanToInt.cpp
@@ -7,19 +7,19 @@ using std::string;
 // example: XII
 int romanToInt(const string &romanNum) {
   int integer = 0;
-  for (int i = 0; i < romanNum.size(); ++i) {
+  for (int i = 1; i < romanNum.size(); ++i) {
     if (romanNum[i] == 'V' && romanNum[i - 1] == 'I') {
-      integer += 4;
+      integer += 3;
     } else if (romanNum[i] == 'X' && romanNum[i - 1] == 'I') {
-      integer += 9;
+      integer += 8;
     } else if (romanNum[i] == 'L' && romanNum[i - 1] == 'X') {
-      integer += 40;
+      integer += 30;
     } else if (romanNum[i] == 'C' && romanNum[i - 1] == 'X') {
-      integer += 90;
+      integer += 80;
     } else if (romanNum[i] == 'D' && romanNum[i - 1] == 'C') {
-      integer += 400;
+      integer += 300;
     } else if (romanNum[i] == 'M' && romanNum[i - 1] == 'C') {
-      integer += 900;
+      integer += 800;
     } else {
       switch (romanNum[i]) {
       case 'I':
@@ -54,6 +54,7 @@ int main() {
   string userInput;
   cout << "Enter Roman number to convert to an integer: ";
   cin >> userInput;
+  userInput = " " + userInput;
   romanToInt(userInput);
   return 0;
 }
