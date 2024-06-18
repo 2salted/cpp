a200ab851b0733c2ba83b0cae8b2f7fb1003570f
2salted <noamgraf123@gmail.com>
2024-06-18 23:31:09 +0000

    added new work

--- june/18-June-2024/palindrome.cc
+++ june/18-June-2024/palindrome.cc
@@ -1,12 +1,30 @@
 #include <iostream>
-#include <string>
-using std::string;
 
-// homie
-bool isPalindrome(int &x) {}
+bool isPalindrome(int &x) {
+  bool isNumPali = false;
+  int reversedNum = 0;
+  int originalNum = x;
+  if (0 > x) {
+    return false;
+  }
+  while (x != 0) {
+    int remainder = x % 10;
+    reversedNum = reversedNum * 10 + remainder;
+    x = x / 10;
+  }
+  if (reversedNum == originalNum) {
+    isNumPali = true;
+    std::cout << "is Palindrome";
+  } else {
+    std::cout << "is not Palindrome";
+  }
+  return isNumPali;
+}
 
 int main() {
-  int num = 123321;
-  isPalindrome(num);
+  int userInput;
+  std::cout << "Enter a number: ";
+  std::cin >> userInput;
+  isPalindrome(userInput);
   return 0;
 }
