b89d7bed7792a97d47a496a5937152a5598e308a
2salted <noamgraf123@gmail.com>
2024-06-22 10:19:29 +0000

    added new date

---
+++ june/22-June-2024/sumSeriesAdd.cpp
@@ -0,0 +1,11 @@
+// Write a program to calculate the series (1) + (1+2) + (1+2+3) + (1+2+3+4) +
+// ... + (1+2+3+4+...+n).
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+  unsigned int nth, sum = 0;
+  cout << "Enter the nth term: ";
+}
