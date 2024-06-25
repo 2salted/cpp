fd965b143c132500777e3379826b30a5b0b64b26
2salted <noamgraf123@gmail.com>
2024-06-25 18:29:47 +0000

    sum of natural num

---
+++ june/25-June-2025/naturalNum.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  unsigned int term;
+  cout << "Enter number of terms: ";
+  cin >> term;
+  cout << term * (term + 1) << endl;
+}
--- june/25-June-2025/sumOfOddNum.cpp
+++ june/25-June-2025/sumOfOddNum.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 int main() {
-  unsigned int nth, count = 1, i = 1;
+  unsigned int nth, count = 1, i = 1, sum = 0;
   cout << "Enter nth term of odd numbers to sum up: ";
   cin >> nth;
   while (count <= nth) {
     if (i % 2 != 0) {
       cout << i << " ";
+      sum += i;
       ++count;
     }
     ++i;
