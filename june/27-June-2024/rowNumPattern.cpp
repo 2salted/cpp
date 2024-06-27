76b287a6045810f7c8bfae2f556d743f11ff99c0
2salted <noamgraf123@gmail.com>
2024-06-27 21:12:08 +0000

    added 2 patterns

---
+++ june/27-June-2024/rowNumPattern.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  uint32_t rows, count = 1;
+  cout << "Enter number of rows: ";
+  cin >> rows;
+  for (uint32_t i = 1; i <= rows; ++i) {
+    for (uint32_t j = 1; j <= count; ++j) {
+      cout << i;
+    }
+    ++count;
+    cout << "\n";
+  }
+  return 0;
+}
