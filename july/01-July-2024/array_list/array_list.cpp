fd231ad13607daa29a99a1fbd98ae27029516d71
Hiten Tandon <tandonhiten@gmail.com>
2024-07-03 04:58:34 +0530

    stuff

--- july/01-July-2024/array_list/array_list.cpp
+++ july/01-July-2024/array_list/array_list.cpp
@@ -7,14 +7,14 @@ using int32 = int32_t;
 using usize = size_t;
 
 ArrayList::ArrayList() {
-  data = new int[16];
-  size = 0;
-  capacity = 16;
+  this->data = new int[16];
+  this->size = 0;
+  this->capacity = 16;
 }
 ArrayList::ArrayList(usize size) {
-  data = new int[size];
-  size = 0;
-  capacity = size;
+  this->data = new int[size];
+  this->size = 0;
+  this->capacity = size;
 }
 ArrayList::ArrayList(usize size, int32 val)
     : data(new int32[size]), size(size), capacity(size) {
