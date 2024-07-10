c21a52791c330b904742b7ff028dcecc6ea4caea
2salted <noamgraf123@gmail.com>
2024-07-10 23:27:51 +0000

    added -1

--- july/10-July-2024/palindromeRecursion.cpp
+++ july/10-July-2024/palindromeRecursion.cpp
@@ -15,8 +15,8 @@ bool palindrome(const vector<int> &arr, int left, int right) {
 }
 
 int main() {
-  vector<int> arr = {1, 2, 3, 2, 1};
-  unsigned int left = 0, right = arr.size();
+  vector<int> arr = {1, 2, 3, 3, 2, 1};
+  unsigned int left = 0, right = arr.size() - 1;
   if (palindrome(arr, left, right)) {
     cout << "is a palindrome" << endl;
   } else {
