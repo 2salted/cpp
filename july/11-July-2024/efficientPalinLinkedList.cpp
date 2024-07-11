5725b78c6502e251e84dbbf0107ee381f63446ff
Hiten Tandon <tandonhiten@gmail.com>
2024-07-11 13:09:24 +0530

    Feat: Linked List stuff

--- july/11-July-2024/efficientPalinLinkedList.cpp
+++ july/11-July-2024/efficientPalinLinkedList.cpp
@@ -1,4 +1,6 @@
 #include <cstdint>
+#include <iostream>
+#include <ostream>
 class ListNode {
 public:
   int32_t val;
@@ -13,13 +15,34 @@ ListNode *middle(ListNode *slow, ListNode *fast) {
   return middle(slow->next, fast->next->next);
 }
 
-ListNode *reverseHelper(ListNode *prev, ListNode *curr) {}
+ListNode *reverse_helper(ListNode *prev, ListNode *curr) {
+  if (curr == nullptr) {
+    return prev;
+  }
 
-ListNode *reverse(ListNode *list) {}
+  ListNode *res = reverse_helper(curr, curr->next);
+  curr->next = prev;
+  return res;
+}
+
+ListNode *reverse(ListNode *list) { return reverse_helper(nullptr, list); }
+
+bool is_equal(ListNode *l1, ListNode *l2) {
+  return l1 == nullptr || l2 == nullptr ||
+         l1->val == l2->val && is_equal(l1->next, l2->next);
+}
 
 bool is_palindrome(ListNode *list) {
   ListNode *par = middle(list, list);
-  ListNode *second = par->next;
+  ListNode *second = reverse(par->next);
   par->next = nullptr;
-  return false;
+  return is_equal(list, second);
+}
+
+int main() {
+  ListNode *list = new ListNode{
+      1, new ListNode{
+             2, new ListNode{3, new ListNode{2, new ListNode{1, nullptr}}}}};
+  std::cout << is_palindrome(list) << std::endl;
+  return 0;
 }
