68042c0ab998875f68bd9b22292e7f706c0fb84d
2salted <noamgraf123@gmail.com>
2024-07-13 09:03:44 +0000

    added cyclic linked list

---
+++ july/13-July-2024/CyclicLinkedList.cpp
@@ -0,0 +1,65 @@
+// Singly Linked Cyclic Linked List
+// 1 -> 2 -> 3 -> 4 -> 5 -+
+//           ^    Cycle   |
+//           +------------+
+//
+// Doubly Linked Cyclic Linked List
+// 1 <-> 2 <-> 3 <-> 4 <-> 5
+// ^                       ^
+// +-----------------------+
+//
+// Circular Linked List
+// 1 -> 2 -> 3 -> 4 -> 5
+// ^                   |
+// +-------------------+
+
+#include <cstdint>
+class ListNode {
+public:
+  int32_t val;
+  ListNode *next;
+};
+
+bool has_cycle(ListNode *list) {
+  ListNode *slow = list;
+  ListNode *fast = list;
+
+  while (fast != nullptr && fast->next != nullptr) {
+    slow = slow->next;
+    fast = fast->next->next;
+
+    if (slow == fast) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+void create_cycle(ListNode *list, int32_t pos) {
+  if (list == nullptr) {
+    return;
+  }
+
+  ListNode *end = list;
+
+  while (end->next != nullptr) {
+    end = end->next;
+  }
+
+  while (pos-- > 0) {
+    list = list->next;
+    end->next = list;
+  }
+}
+
+int main() {
+  ListNode *list = new ListNode{
+      0,
+      new ListNode{
+          1,
+          new ListNode{
+              2, new ListNode{3, new ListNode{4, new ListNode{5, nullptr}}}}}};
+
+  create_cycle(list, 3);
+};
