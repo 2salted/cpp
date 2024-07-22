ec21a01f4e1b1c910cc5512a9142ded0ea737e13
2salted <noamgraf123@gmail.com>
2024-07-22 13:59:08 +0000

    update

--- july/22-July-2024/longestCommonPrefix.cpp
+++ july/22-July-2024/longestCommonPrefix.cpp
@@ -9,12 +9,11 @@ public:
   string longestCommonPrefix(vector<string> &strs) {
     string subStr = strs[0];
     for (uint32_t i = 1; i < strs.size(); ++i) {
-      for (uint32_t j = 0; j < strs[i].size(); ++j) {
+      for (uint32_t j = 0; j < min(strs[i].size(), subStr.size()); ++j) {
         if (subStr[j] != strs[i][j]) {
           subStr = strs[i].substr(0, j);
         }
       }
-      cout << subStr << "\n";
     }
     return subStr;
   }
