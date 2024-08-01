339b91ab7aedbbc934b57d03e2b2a4e9c2d2be03
2salted <noamgraf123@gmail.com>
2024-08-01 06:39:19 +0000

    new file

---
+++ july/31-July-2024/richestCustomerWealth.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int maximumWealth(vector<vector<int>> const &accounts) {}
+};
+
+int main() {
+  Solution a;
+  a.maximumWealth({{1, 2, 3}, {4, 8, 1}, {0, 2, 5}});
+}
