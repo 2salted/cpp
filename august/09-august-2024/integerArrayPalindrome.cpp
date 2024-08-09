61a24923e877fcb689619fb5de89ba4a916d599b
2salted <noamgraf123@gmail.com>
2024-08-09 13:33:23 +0000

    added array integer palindrome checking program

--- august/09-august-2024/integerArrayPalindrome.cpp
+++ august/09-august-2024/integerArrayPalindrome.cpp
@@ -20,24 +20,40 @@ public:
   }
 
   int useFirstHalf(vector<int> const &arr) {
-    int res = 0;
+    int numRes = 0;
     int testNum = 0;
-    for (int i = arr.size() / 2;) {
+    for (int i = (arr.size() - 2) / 2; 0 <= i; --i) {
+      testNum = arr[i];
+      while (testNum != 0) {
+        int takeLast = testNum % 10;
+        numRes *= 10;
+        numRes += takeLast;
+        testNum /= 10;
+      }
     }
+    int finalResult = 0;
+    while (numRes != 0) {
+      int takeLast = numRes % 10;
+      finalResult *= 10;
+      finalResult += takeLast;
+      numRes /= 10;
+    }
+    return finalResult;
   }
 
-  bool isPalindrome(vector<int> const &arr) {}
+  bool isPalindrome(vector<int> const &arr) {
+    int firstHalf = useFirstHalf(arr);
+    int secondHalf = reverseSecondHalf(arr);
+    if (firstHalf / 10 == secondHalf || secondHalf / 10 == firstHalf ||
+        firstHalf == secondHalf) {
+      return true;
+    }
+    return false;
+  }
 };
 
 int main() {
   Solution s;
-  cout << s.reverseSecondHalf({412, 23, 2, 21, 4});
+  cout << s.isPalindrome({41, 12, 1, 21, 41});
   return 0;
 }
-//
-//
-//
-//
-//
-//
-//
